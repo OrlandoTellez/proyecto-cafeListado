b86b2c64345d3ae76b72eec3fe1b84ad881d54cf
OrlandoTellez <espadachinkinno@gmail.com>
2024-02-28 20:19:22 -0600

    add: se hacen todos los cambios

--- src/components/Tarjetas.jsx
+++ src/components/Tarjetas.jsx
@@ -1,23 +1,26 @@
-import "./Tarjetas.css";
-import imagenes from "./imagenes"
+import { useFetch } from "./useFetch"
+import "./Tarjetas.css"
 
 export const Tarjetas = () => {
-   
+  const {data} = useFetch("https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json")
+
   return (
     <>
-        <div className="container-tarjeta">
-          <div className="container-tarjeta__contenido">
-            <figure>
-              <img src={imagenes.img1} alt="" />
-            </figure>
-            <div>
-              <p>Capuccino</p>
-              <span>0 Usd</span>
+        <div className="container">
+          {data?.map((item) => (
+            <div key={item.id} className="container-card">
+              <figure>
+                <img src={item.image} alt={item.name} />
+              </figure>
+              <div className="container-text">
+                <p>{item.name}</p>
+                <span>{item.price}</span>
+              </div>
+              <div className="container-rating">
+                <p>${item.rating} <span className="vote">({item.votes} votes)</span> </p>
+              </div>
             </div>
-            <div>
-              <p>4.7</p>
-            </div>
-          </div>
+          ))}
         </div>
     </>
   )
--- src/components/imagenes.js
+++
@@ -1,5 +0,0 @@
-import imagen1 from "../assets/cafe2.jpg"
-
-export default{
-    "img1": imagen1
-}
